56120012520f82063b4ba5a3c71df96e7db9be9c
Guillaume ARM <trapcodien@gmail.com>
2018-03-13 02:08:01 +0100

    feat(dummy): use pipe oprator in empty.js
    pipe operator is a babel stage-0 feature (according to tc39 proposal)

--- src/empty.js
+++ src/empty.js
@@ -1,1 +1,4 @@
-void null;
+
+void (41 |> x => x + 1);
+void (41 |> x => x + 1);
+void (41 |> x => x + 1);
--- src/index.js
+++ src/index.js
@@ -7,6 +7,8 @@ const readJsonFile = async (filePath) => {
   return JSON.parse(await readFile(filePath, 'utf8'))
 }
 
+import './empty';
+
 module.exports = async function main() {
   const packageJsonPath = resolve(__dirname, '..', 'package.json');
   const { version } = await readJsonFile(packageJsonPath);
